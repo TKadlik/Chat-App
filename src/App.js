dbe55c99175f65dc1dd63c9f97ede6c1db636840
TKadlik <tomislav.kadlik@gmail.com>
2023-07-26 22:00:19 +0200

    modified:   src/App.css 	modified:   src/App.js 	modified:   src/index.css 	modified:   src/index.js 	modified:   package-lock.json 	modified:   package.json 	deleted:    public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	public/svg/ 	src/components/ 	src/images/ 	src/media/ 	src/utilities/

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Registration from "./components/Registration";
+import Fullscreen from "./components/Fullscreen";
+import Header from "./components/Header";
+import Messages from "./components/Messages";
+import Input from "./components/Input";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const initialChatState = {
+    member: { username: "", avatar: "" },
+    messages: []    
+  };
+
+  const [chat, setChat] = useState(initialChatState);
+  const [drone, setDrone] = useState(null);
+
+  useEffect(() => {
+    if (chat.member.username !== "") {
+      const drone = new window.Scaledrone("cgyxqNwNjsS5MJKa", {
+        data: chat.member
+      });
+      setDrone(drone);
+    }
+  }, [chat.member]);
+
+  useEffect(() => {
+    if (chat.messages.length) {
+      const scrollElement = document.getElementsByClassName("msg-list")[0];
+      scrollElement.scrollTop = scrollElement.scrollHeight;
+    }
+  }, [chat.messages.length]);
+
+  if (drone) {
+    drone.on("open", (error) => {
+      if (error) {
+        return console.error(error);
+      }
+      chat.member.id = drone.clientId;
+      setChat({ ...chat }, chat.member);
+
+      const room = drone.subscribe("observable-room");
+
+      room.on("message", (message) => {
+        const { data, member, timestamp, id } = message;
+        chat.messages.push({ member, data, timestamp, id });
+        setChat({ ...chat }, chat.messages);
+      });
+    });
+  }
+
+  const onSendMessage = (message) => {
+    drone.publish({
+      room: "observable-room",
+      message
+    });
+  };
+
+  const handleRegFormSubmit = (username, avatar) => {
+    chat.member = {
+      username: username,
+      avatar: avatar
+    };
+    setChat({ ...chat }, chat.member);
+  };
+
+  return chat.member.username === "" ? (
+    <Registration handleRegFormSubmit={handleRegFormSubmit} />
+  ) : (
+    <div className="chat">
+      <Header />
+      <Fullscreen />
+      <Messages messages={chat.messages} thisMember={chat.member} />
+      <Input onSendMessage={onSendMessage} />
     </div>
   );
 }
-
-export default App;
